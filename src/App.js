fb55ebc2e370ca2ce83ebd16fe7ae9ff3ae58a68
Ricardo <rmedina0531@gmail.com>
2021-01-16 15:17:23 -0800

    navbar finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
